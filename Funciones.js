63783fe14dab5f5792b8f56d28e3ada72cf15e3e
belenaguilarv <belenaguiilarv@gmail.com>
2021-06-15 19:40:08 -0300

    Se modificaron alertas en js

--- Funciones.js
+++ Funciones.js
@@ -22,9 +22,22 @@ function calcularAltura()
         document.getElementById("gravedad").value = "";
     }
     else if(Vi > 51.5 || T > 3 || G < 9.80 || G > 10){
-        alert("Los ingresados superan el limite");
+        alert("Valores invalidos, ingresar nuevamente");
         document.getElementById("velocidadInicial").value = "";
         document.getElementById("tiempo").value = "";
+        document.getElementById("gravedad").value = "";
+    }
+    else if(Vi > 51.5) {
+        alert("Ingrese una velocidad inicial positiva menor o igual a 51.5");
+        document.getElementById("velocidadInicial").value = "";
+    }
+    else if(T > 3) {
+        alert("Ingrese un valor para el tiempo positivo  menor o igual a 3")
+        document.getElementById("tiempo").value = "";
+    }
+    else if(G < 9.80 || G > 10)
+    {
+        alert("Ingrese una gravedad entre 9.8 y 10 ");
         document.getElementById("gravedad").value = "";
     }
     else{
